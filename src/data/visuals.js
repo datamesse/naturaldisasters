8f948c44f8222d10ca9be9ea4cc0bda5c92ba426
datamesse <48945612+datamesse@users.noreply.github.com>
2022-03-02 18:23:32 +1100

    Add assets

--- src/data/visuals.js
+++ src/data/visuals.js
@@ -3,7 +3,7 @@
 import vbar from '../visuals/bars.vl.json';
 import vcolumn from '../visuals/columns.vl.json';
 import vline from '../visuals/line.vl.json';
-import vmap from '../visuals/map.vl.json';
+import vmap from '../visuals/map.v.json';
 import vtimeline from '../visuals/timeline.v.json';
 
 export { vbar, 
